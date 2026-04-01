8ab30cb128685540c2d65addfe9f2e60d370939a
sachinbisht577 <sachinbisht5577@gmail.com>
2026-04-01 23:07:59 +0530

    Initialize Express app with Prometheus metrics

---
+++ app/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+const register = new client.Registry();
+
+client.collectDefaultMetrics({ register });
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+});
+
+register.registerMetric(httpRequestCounter);
+
+app.get('/', (req, res) => {
+  httpRequestCounter.inc();
+  res.send('Hello DevOps Monitoring 🚀');
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+app.listen(3000, () => console.log('App running on port 3000'));
